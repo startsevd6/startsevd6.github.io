9cdb2059c84fa60884511afb944407e2882190fe
startsevd6 <startsevd6@gmail.com>
2023-05-14 15:25:51 +0700

    v.0.0.3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //alert("Сайт в разработке");
-$('document').ready(function() {
+$('document').ready(function () {
     const aside = $('aside');
     const sections = $('.sections');
     const nameLesson = $('.name-lesson');
@@ -8,15 +8,15 @@ $('document').ready(function() {
     const aLesson = $('.a-lesson');
     const aSettings = $('.a-settings');
     const buttonMenu = $('.button-menu');
-    
+
     // Расширяем сайдбар при нажатии на кнопку
-    $(".button-menu").on("click", function() {
+    $(".button-menu").on("click", function () {
         // если сайдбар сжат, то расширяем, если нет, то сжимаем
         if (aside.width() === 70) {
             aside.toggleClass('animate');
             aside.toggleClass('open');
             sections.toggleClass('shifted');
-            setTimeout(function() {
+            setTimeout(function () {
                 aLesson.addClass('extension');
                 aSettings.addClass('extension');
                 buttonMenu.addClass('extension');
@@ -32,7 +32,7 @@ $('document').ready(function() {
             buttonMenu.removeClass('extension');
             nameLesson.removeClass('visible');
             nameMenu.removeClass('visible');
-            afterImg.removeClass('visible');    
+            afterImg.removeClass('visible');
         }
 
         // текст кнопок появляется после анимации
@@ -44,9 +44,9 @@ $('document').ready(function() {
             afterImg.toggleClass('visible');
         });*/
     });
-    
+
     // Возвращам ширину сайдбару, если ширина окна стала больше
-    $(window).resize(function() {
+    $(window).resize(function () {
         if ($(window).width() >= 1200 || $(window).width() <= 950) {
             aside.removeAttr('style');
             aside.removeClass('open');
@@ -63,7 +63,7 @@ $('document').ready(function() {
 
     // Функция для копирования кода
     new ClipboardJS('.copy-code-lesson-1', {
-        text: function() {
+        text: function () {
             try {
                 const codeElement = document.querySelector('#code-1');
                 return codeElement.innerText;
@@ -71,14 +71,14 @@ $('document').ready(function() {
                 alert('Ошибка: не удалось найти текст для копирования');
             }
         }
-    }).on('error', function(e) {
+    }).on('error', function (e) {
         alert('Ошибка: не удалось скопировать текст');
     })
 
     // Плавный переход к блоку текста
-    $('a[href^="#"]').on('click', function(event) {
+    $('a[href^="#"]').on('click', function (event) {
         var target = $(this.getAttribute('href'));
-        if( target.length ) {
+        if (target.length) {
             event.preventDefault();
             $('html, body').animate({
                 scrollTop: target.offset().top - 100

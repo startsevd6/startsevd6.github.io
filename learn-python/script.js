f196985a003b1bc86b2ad4a4699bce1dff5b3f84
startsevd6 <startsevd6@gmail.com>
2023-05-17 18:44:19 +0700

    v.0.1.3b

--- learn-python/script.js
+++ learn-python/script.js
@@ -1,72 +1,126 @@
-//alert("Сайт в разработке");
-$('document').ready(function () {
+// сайт в альфа версии
+
+let sectionsIsShifted = false;
+
+// Расширяем сайдбар при нажатии на кнопку
+$(document).on('click', '.button-menu', function () {
+    // объявляем локальные переменные и функции, т.к. кнопка подружается асинхронно
     const aside = $('aside');
     const sections = $('.sections');
-    const nameLesson = $('.name-lesson');
-    const nameMenu = $('.name-menu');
-    const afterImg = $('#after-img');
+
     const aLesson = $('.a-lesson');
     const aSettings = $('.a-settings');
     const buttonMenu = $('.button-menu');
+    const nameLesson = $('.name-lesson');
+    const nameMenu = $('.name-menu');
+    const afterImg = $('#after-img');
+
+    function addClasses() {
+        aLesson.addClass('extension');
+        aSettings.addClass('extension');
+        buttonMenu.addClass('extension');
+        nameLesson.addClass('visible');
+        nameMenu.addClass('visible');
+        afterImg.addClass('visible');
+    }
+
+    function removeClasses() {
+        aLesson.removeClass('extension');
+        aSettings.removeClass('extension');
+        buttonMenu.removeClass('extension');
+        nameLesson.removeClass('visible');
+        nameMenu.removeClass('visible');
+        afterImg.removeClass('visible');
+    }
+
+    // если сайдбар сжат, то расширяем, если нет, то сжимаем
+    if (aside.width() === 70) {
+        aside.addClass('animate');
+        aside.toggleClass('open');
+        sections.addClass('animate');
+        sections.addClass('shifted');
+        sectionsIsShifted = true;
+        setTimeout(function () {
+            addClasses();
+        }, 500);
+    } else {
+        aside.removeClass('open');
+        sections.removeClass('shifted');
+        sectionsIsShifted = false;
+        removeClasses();
+    }
+});
+
+$(document).ready(function () { 
+    const includeAside = $('#include-aside');
+    const includeFooter = $('#include-footer');
 
     // Подгружаем aside из отдельного файла
-    $("#include-aside").load('/learn-python/aside.html'), function () {
-        alert("Загрузка боковой панели не удалась");
-    };
-
-    // Подгружаем footer из отдельного файла
-    $("#include-footer").load('/learn-python/footer.html'), function () {
-        alert("Загрузка нижнего блока сайта не удалась");
-    };
-
-    // Расширяем сайдбар при нажатии на кнопку
-    $(".button-menu").on("click", function () {
-        // если сайдбар сжат, то расширяем, если нет, то сжимаем
-        if (aside.width() === 70) {
-            aside.toggleClass('animate');
-            aside.toggleClass('open');
-            sections.toggleClass('shifted');
-            setTimeout(function () {
-                aLesson.addClass('extension');
-                aSettings.addClass('extension');
-                buttonMenu.addClass('extension');
-                nameLesson.addClass('visible');
-                nameMenu.addClass('visible');
-                afterImg.addClass('visible');
-            }, 500);
-        } else {
-            aside.removeClass('open');
-            sections.removeClass('shifted');
-            aLesson.removeClass('extension');
-            aSettings.removeClass('extension');
-            buttonMenu.removeClass('extension');
-            nameLesson.removeClass('visible');
-            nameMenu.removeClass('visible');
-            afterImg.removeClass('visible');
+    includeAside.load('/learn-python/aside.html', function (response, status, xhr) {
+        if (status === "error") {
+            alert("Загрузка боковой панели не удалась");
         }
     });
 
-    // Возвращам ширину сайдбару, если ширина окна стала больше
-    $(window).resize(function () {
-        if ($(window).width() >= 1200 || $(window).width() <= 950) {
-            aside.removeAttr('style');
-            aside.removeClass('open');
-            aside.removeClass('animate');
-            sections.removeClass('')
-            aLesson.removeClass('extension');
-            aSettings.removeClass('extension');
-            buttonMenu.removeClass('extension');
-            nameLesson.removeClass('visible');
-            nameMenu.removeClass('visible');
-            afterImg.removeClass('visible');
+    // Подгружаем footer из отдельного файла
+    includeFooter.load('/learn-python/footer.html', function (response, status, xhr) {
+        if (status === "error") {
+            alert("Загрузка нижнего блока сайта не удалась");
         }
+    });
+
+    const aside = $('aside');
+    const sections = $('.sections');
+
+    const aLesson = $('.a-lesson');
+    const aSettings = $('.a-settings');
+    const buttonMenu = $('.button-menu');
+    const nameLesson = $('.name-lesson');
+    const nameMenu = $('.name-menu');
+    const afterImg = $('#after-img');
+    // созда1м отдельные функции addClasses() и removeClasses() чтобы не дублировать код
+    function addClasses() {
+        aLesson.addClass('extension');
+        aSettings.addClass('extension');
+        buttonMenu.addClass('extension');
+        nameLesson.addClass('visible');
+        nameMenu.addClass('visible');
+        afterImg.addClass('visible');
+    }
+
+    function removeClasses() {
+        aLesson.removeClass('extension');
+        aSettings.removeClass('extension');
+        buttonMenu.removeClass('extension');
+        nameLesson.removeClass('visible');
+        nameMenu.removeClass('visible');
+        afterImg.removeClass('visible');
+    }
+
+    // Возвращам ширину сайдбару, если ширина окна стала больше
+    let debounceTimeout;
+    $(window).resize(function () {
+        clearTimeout(debounceTimeout);
+        debounceTimeout = setTimeout(function () {
+            if ($(window).width() >= 1200 || $(window).width() <= 935) {
+                aside.removeClass('open');
+                aside.removeClass('animate');
+                sections.removeClass('shifted');
+                removeClasses();
+            } else {
+                if (sectionsIsShifted == true) {
+                    sections.addClass('shifted');aside.addClass('open');
+                }
+                setTimeout(function () {
+                    sections.addClass('animate');aside.addClass('animate');
+                }, 500);
+                addClasses();
+            }
+        }, 100);
     });
 
     // Функция для копирования кода
-    // Получите все кнопки с классом "copy-code-button"
     const copyCodeButtons = document.querySelectorAll('.copy-code-lesson');
-
-    // Переберите каждую кнопку и настройте ClipboardJS для неё
     copyCodeButtons.forEach(function (button) {
         new ClipboardJS(button, {
             text: function (trigger) {
@@ -82,7 +136,7 @@ $('document').ready(function () {
 
     // Плавный переход к блоку текста
     $('a[href^="#"]').on('click', function (event) {
-        var target = $(this.getAttribute('href'));
+        let target = $(this.getAttribute('href'));
         if (target.length) {
             event.preventDefault();
             $('html, body').animate({

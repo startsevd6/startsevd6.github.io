fe7c5f584513194305ec510a7e8cc4d04900d30b
startsevd6 <startsevd6@gmail.com>
2023-09-06 11:01:20 +0700

    v.0.5.2.0 completely changed the method of compressing css code

--- code-minifier/script.js
+++ code-minifier/script.js
@@ -1,101 +1,41 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-    function compressCSS(css) {
-        let compressedCSS = '';
-        let inComment = false;
-        let inWhitespace = false;
+    function removeCommentsAndWhitespace(css) {
+        // Регулярные выражения для удаления комментариев и лишних пробелов
+        css = css.replace(/\/\*[\s\S]*?\*\//g, ''); // Удаление комментариев
+        css = css.replace(/\s+/g, ' '); // Удаление лишних пробелов
+        css = css.replace(/;\s*}/g, '}'); // Удаление пробелов перед '}' после ';'
 
-        for (let i = 0; i < css.length; i++) {
-            const currentChar = css[i];
-            const nextChar = css[i + 1];
-
-            if (inComment) {
-                if (currentChar === '*' && nextChar === '/') {
-                    inComment = false;
-                    i++; // Пропускаем символ '/'
-                }
-                continue;
-            }
-
-            if (currentChar === '/' && nextChar === '*') {
-                inComment = true;
-                i++; // Пропускаем символ '*'
-                continue;
-            }
-
-            if (/\s/.test(currentChar)) {
-                if (!inWhitespace) {
-                    compressedCSS += ' ';
-                    inWhitespace = true;
-                }
-                continue;
-            }
-
-            if (/[\,\;\}]/.test(currentChar)) {
-                console.log(currentChar, nextChar);
-                // Пропускаем пробелы после ',', ';', '}'
-                i++; // Пропускаем символ пробела
-                compressedCSS += currentChar;
-                inWhitespace = false;
-                continue;
-            }
-
-            if (currentChar === '{' || currentChar === '}') {
-                compressedCSS = compressedCSS.replace(/\s+$/, ''); // Убираем пробелы перед '{' и '}'
-                compressedCSS += currentChar;
-                if (currentChar === '}') {
-                    if (nextChar === ' ') {
-                        i++;
-                    }
-                    inWhitespace = false;
-                }
-                continue;
-            }
-
-            if ((currentChar === ':' || currentChar === ';') && nextChar === ' ') {
-                compressedCSS += currentChar;
-                i++;
-                continue;
-            }
-
-            inWhitespace = false;
-            compressedCSS += currentChar;
-        }
-
-        return compressedCSS.trim();
+        return css.trim();
     }
 
+    function compressCSS(css) {
+        // Ваш код сжатия CSS
+
+        return css.trim();
+    }
 
     function compressAndDisplay() {
         const textareaOriginal = document.getElementById('original-code');
-        if (!textareaOriginal) {
-            console.error('Textarea for original code with id "code" not found.');
-            return;
-        }
-
         const textareaCompressed = document.getElementById('minified-code');
-        if (!textareaCompressed) {
-            console.error('Textarea for minified code with id "code" not found.');
+        const labelMinified = document.querySelector('label.minified.code');
+
+        if (!textareaOriginal || !textareaCompressed || !labelMinified) {
+            console.error('One or more elements not found.');
             return;
         }
 
         const originalCSS = textareaOriginal.value;
-        const compressedCSS = compressCSS(originalCSS);
+        const compressedCSS = compressCSS(removeCommentsAndWhitespace(originalCSS));
         textareaCompressed.value = compressedCSS;
-
-        const labelMinified = document.querySelector('label.minified.code');
-        if (!textareaCompressed) {
-            console.error('Label for minified code with class "minified code" not found.');
-            return;
-        }
 
         labelMinified.innerHTML = `Сжатый код (Сжато на ${originalCSS.length - compressedCSS.length} байт(ов), это ${Math.trunc(100 - (compressedCSS.length / originalCSS.length * 100))}%)`;
     }
 
     const button = document.querySelector('button');
 
-    button.addEventListener('click', () => {
-        compressAndDisplay();
-    });
+    if (button) {
+        button.addEventListener('click', compressAndDisplay);
+    }
 });
